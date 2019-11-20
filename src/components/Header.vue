60fdda02d052bf0622045c6d6f752c2e6d6a5a28
2ndyrGroupC <2ndyrGroupC@pn.local>
2019-11-20 12:23:22 +0800

    INitial pa ni

---
+++ src/components/Header.vue
@@ -0,0 +1,80 @@
+<template>
+  <div>
+    <v-system-bar color="deep-purple darken-3"></v-system-bar>
+
+    <v-app-bar color="deep-purple accent-4" dark prominent>
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+
+      <v-spacer></v-spacer>
+
+      <v-btn icon>
+        <v-icon>mdi-magnify</v-icon>
+      </v-btn>
+    </v-app-bar>
+
+    <v-navigation-drawer v-model="drawer" absolute bottom temporary height="50%">
+      <v-list nav dense>
+        <v-list-item-group v-model="group" active-class="deep-purple--text text--accent-4">
+          <v-avatar size="100" style="margin-left:30%">
+         
+
+            <img src="https://cdn.vuetifyjs.com/images/john.jpg" alt="John">
+          </v-avatar>
+          <center><h2>WeOrg!</h2></center>
+           <br> 
+
+          <hr>
+
+          <v-list-item>
+            <router-link to="/Home">HOME</router-link>
+          </v-list-item>
+
+          <v-list-item>
+            <router-link to="/About">ABOUT</router-link>
+          </v-list-item>
+
+          <div>
+            <v-menu>
+              <template v-slot:activator="{ on }">
+                <v-btn dark v-on="on">CATEGORIES</v-btn>
+              </template>
+              <v-list>
+                <v-list-item v-for="(item, index) in items" :key="index">
+                  <v-list-item-title>{{ item.title }}</v-list-item-title>
+                </v-list-item>
+              </v-list>
+            </v-menu>
+          </div>
+          <v-list-item>
+            <router-link to="/Login">LOGIN</router-link>
+          </v-list-item>
+          <v-list-item>
+            <router-link to="/Signup">SIGNUP</router-link>
+          </v-list-item>
+        </v-list-item-group>
+      </v-list>
+    </v-navigation-drawer>
+  </div>
+</template>
+<script>
+export default {
+  data: () => ({
+    drawer: false,
+    group: null,
+    items: [
+      { title: "Birthday" },
+      { title: "Burial" },
+      { title: "Baptist" },
+      { title: "Wedding" }
+    ]
+  }),
+
+  watch: {
+    group() {
+      this.drawer = false;
+    }
+  }
+};
+</script>
+
+
---
+++ src/views/Signup.vue
@@ -0,0 +1,46 @@
+<template>
+  <v-app class="form" id="inspire">
+    <v-content >
+      <v-container style="margin-left:30%">
+        <v-row align="center" justify="center">
+          <v-col cols="12" sm="8" md="4">
+            <v-card class="elevation-12">
+              <v-toolbar color="secondary" dark flat>
+                <v-toolbar-title >SIGNUP</v-toolbar-title>
+                <v-spacer/>
+           </v-toolbar>
+              <v-card-text>
+                <v-form>
+                  <v-text-field label="Name " name="name" type="text"/>
+                  <v-text-field id="address" label="Address" name="address" type="text"/>
+                   <v-text-field label="Email " name="email" type="email"/>
+                  <v-text-field id="password" label="Password" name="password" type="password"/>
+                   <v-text-field label="Contact Number " name="contact" type="number"/>
+                  <v-text-field id="event" label="Event" name="event" type="text"/>
+                    <v-text-field label="Range" name="range" type="number"/>
+                  <v-text-field id="package" label="Package" name="package" type="text"/>
+                   <v-text-field id="photo"  name="photo" type="file"/>
+                </v-form>
+              </v-card-text>
+              <v-card-actions>
+                <v-spacer/>
+                <v-btn color="secondary">Login</v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+export default {
+  props: {
+    source: String
+  }
+};
+</script>
+<style scoped>
+
+</style>
